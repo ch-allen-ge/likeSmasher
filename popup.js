e8b63a11c47b1781071f7a8f21a138654bb5e6e3
Allen Tan <d4rkmage80@gmail.com>
2020-07-26 15:43:45 -0400

    1. Added jquery 2. Added styling to row 3. Added functionality that saves row data to array, on enter key click or submit button

--- popup.js
+++ popup.js
@@ -5,6 +5,15 @@ var port = chrome.extension.connect({
 });
 
 var allInputs = document.querySelectorAll("input");
+var allSaveButtons = document.querySelectorAll("button");
+
+function createNewRow() {
+
+}
+
+function generateStaticRow() {
+    
+}
 
 allInputs.forEach(function(input) {
     input.addEventListener("keyup", function(event) {
@@ -12,6 +21,18 @@ allInputs.forEach(function(input) {
             event.preventDefault();
             channelArray.push(input.value);
             console.log(channelArray);
+            $(this).parent().parent().replaceWith('<div>' + input.value + '</div>');
+            //createNewRow
         }
+    });
+});
+
+allSaveButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        let value = $(this).parent().children('.t').children('input').val();
+        channelArray.push(value);
+        $(this).parent().replaceWith('<div>' + value + '</div>');
+        console.log(channelArray);
+        //createNewRow
     });
 });
